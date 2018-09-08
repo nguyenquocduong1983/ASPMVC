fff731879b477b7f1f0c10629340c16c5a8fb93e
duongnq <nguyenquocduong1983@gmail.com>
2018-09-08 14:08:52 -0700

    2

--- StadyMVC5/StadyMVC5/Controllers/HomeController.cs
+++ StadyMVC5/StadyMVC5/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StadyMVC5.Models;
+using StadyMVC5.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,26 @@ namespace StadyMVC5.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+       public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            List<SiteUserModel> list = new List<SiteUserModel>();
+            using (webgird_dbEntities db = new webgird_dbEntities())
+            {
+                var model = (
+                    from a in db.SiteUsers
+                    join b in db.UserRoles on a.RoleID equals b.ID
+                    select new SiteUserModel
+                    {
+                        ID = a.ID,
+                        FirstName = a.FirstName,
+                        LastName = a.LastName,
+                        DOB = a.DOB,
+                        RoleName = b.RoleName
+                    }
+                    );
+                list = model.ToList();
+            }
+            return View(list);
         }
     }
 }
